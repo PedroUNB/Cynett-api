6fc5ef9c7c5789ff4267eaffcca0ac60da64c905
PedroUNB <pedro.oliveira.unb@gmail.com>
2021-04-27 00:59:27 -0300

    :sparkles: Adição de middlewares

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,10 +1,18 @@
 import express, { Application } from 'express'
+import cors from 'cors'
 class App {
   express: Application
 
   constructor() {
     this.express = express()
+
+    this.middlewares()
   };
+
+  private middlewares() {
+    this.express.use(express.json())
+    this.express.use(cors({ origin: '*' }))
+  }
 }
 
 export default new App().express
